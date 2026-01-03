0cc27ee72481d10d0db912feadc8b3465439b4fb
AKSHAYA K <akshayakrishnamohan@gmail.com>
2026-01-03 17:22:16 +0530

    Add files via upload

---
+++ ev_app/app/tabs/Home.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the Home Screen!</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
